72d3b2c508bb4aa7b8be82f69de2274c0991d4b8
newmsacc999 <newmsacc999@hotmail.com>
2026-06-30 12:55:56 +0530

    Update Payment.jsx

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -43,23 +43,11 @@ const Payment = () => {
       }
     };
     fetchSettings();
-
-    // Load Facebook Pixel SDK if not already loaded
-    if (!window.fbq) {
-      !function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)}(window, document,'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
-    }
   }, []);
 
   if (!product) {
     return (
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex justify-center items-center h-screen bg-white">
         Loading...
       </div>
     );
@@ -127,7 +115,10 @@ const Payment = () => {
   // Track when component mounts
   useEffect(() => {
     if (product) {
-      trackInitiateCheckout();
+      // Small delay to ensure FB Pixel is loaded
+      setTimeout(() => {
+        trackInitiateCheckout();
+      }, 500);
     }
   }, [product]);
 
