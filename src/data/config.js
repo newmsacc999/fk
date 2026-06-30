735a84654b3b63ee67d127d92146c85a909ca22c
newmsacc999 <newmsacc999@hotmail.com>
2026-06-30 11:47:01 +0530

    Update config.js

--- src/data/config.js
+++ src/data/config.js
@@ -19,7 +19,7 @@ export const INITIAL_SETTINGS = {
   pay_type: false, // false = UPI (pay_type_1), true = Common (pay_type_2)
   payment_script: "",
   allowed_ip: "",
-  upi: "ombk.aahr05789wz5v0hswn8@mbk",
+  upi: "cc.9199096092669503@axisbank",
   pixel: "",
   is_maintenance: false, // Added to support client-side maintenance mode
 };
